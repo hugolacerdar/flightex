e3d12a2eb9805ae7703d9ff5ebd8ca88f4ef3ecc
Hugo <hugolacerdar@gmail.com>
2021-05-17 08:17:02 -0300

    Fixing get_all return

--- lib/users/agent.ex
+++ lib/users/agent.ex
@@ -9,7 +9,7 @@ defmodule Flightex.Users.Agent do
 
   def save(%User{} = user), do: Agent.update(__MODULE__, &update_state(&1, user))
 
-  def get_all, do: Agent.get(__MODULE__, & &1)
+  def get_all, do: Agent.get(__MODULE__, &{:ok, &1})
 
   def get(id), do: Agent.get(__MODULE__, &get_user(&1, id))
 
